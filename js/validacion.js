85144c69bcce5fe214d0321b299cff8b24db74b9
SelenaSeidel <selenaluisaseidel@gmail.com>
2024-08-15 19:13:20 -0300

    contrasñas

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,4 @@
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +6,17 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+//chequeo contraseña
+document.addEventListener("DOMContentLoaded", function () {
+    let boton = document.getElementById("regBtn");  
+    boton.addEventListener("click", function(){         //al hacer click 
+        let pass1=document.getElementById("password1");
+        let pass2=document.getElementById("password2");
+        if (pass1.value==pass2.value){   //compara contraseñas
+            showAlertSuccess();
+        }else{
+            showAlertError();
+        }
+  })
+  });
